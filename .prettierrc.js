cc59f7f794916b1df5bf7ab8b7669036caa3b25c
vinit_groww <vinitraj1012@gmail.com>
2020-05-08 15:46:15 +0530

    new eslint config added

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+/*  "prettier": {
+    "quoteProps": "consistent",
+    "singleQuote": true,
+    "tabWidth": 2,
+    "trailingComma": "es5",
+    "useTabs": false
+  }, */
