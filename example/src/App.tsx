08b8edc69eaa538b84b3415e41fab0a39ae15a78
vinit_groww <vinitraj1012@gmail.com>
2020-05-09 15:49:10 +0530

    added navigation in examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,58 @@
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-// eslint-disable-next-line import/no-unresolved
-import { Button } from 'supergroww';
+import { Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  ButtonScreen,
+  RadioButtonScreen,
+  NumPadScreen,
+  TableScreen,
+} from './Examples';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }: any) => {
+  const screens = [
+    'ButtonScreen',
+    'RadioButtonScreen',
+    'NumPadScreen',
+    'TableScreen',
+  ];
   return (
-    <View style={styles.container}>
-      <View>
-        <Button label="Primary" type="primary" />
-      </View>
-    </View>
+    <SafeAreaView>
+      {screens.map((item) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item)}
+          style={styles.navButtonContainer}
+          key={item}
+        >
+          <Text>{item}</Text>
+        </TouchableOpacity>
+      ))}
+    </SafeAreaView>
+  );
+};
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ButtonScreen" component={ButtonScreen} />
+        <Stack.Screen name="RadioButtonScreen" component={RadioButtonScreen} />
+        <Stack.Screen name="NumPadScreen" component={NumPadScreen} />
+        <Stack.Screen name="TableScreen" component={TableScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  navButtonContainer: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(191, 191, 191, 0.5)',
   },
 });
+export default App;
---
+++ example/src/examples/index.tsx
@@ -0,0 +1,4 @@
+export { default as ButtonScreen } from './ButtonExample';
+export { default as RadioButtonScreen } from './RadioButtonExample';
+export { default as NumPadScreen } from './NumPadExample';
+export { default as TableScreen } from './TableExample';
