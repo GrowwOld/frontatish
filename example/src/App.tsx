3c4f996c32dab571ab60c990359fe90714d33c30
vinit_groww <vinitraj1012@gmail.com>
2020-05-11 15:50:07 +0530

    working to integrate vector icons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ButtonScreen from './examples/ButtonExample';
+import ChexboxScreen from './examples/CheckboxExample';
 import RadioButtonScreen from './examples/RadioButtonExample';
 import NumPadScreen from './examples/NumPadExample';
 import TableScreen from './examples/TableExample';
@@ -13,7 +14,15 @@ import PopupScreen from './examples/PopupExample';
 const Stack = createStackNavigator();
 
 const HomeScreen = ({ navigation }: any) => {
-  const screens = ['Button', 'RadioButton', 'NumPad', 'Tables', 'Popup'];
+  const componentScreens = [
+    'Button',
+    'RadioButton',
+    'CheckBox',
+    'NumPad',
+    'Tables',
+    'Popup',
+  ];
+  const animatedScreens = ['Fade'];
   return (
     <SafeAreaView>
       {screens.map((item) => (
@@ -35,6 +44,7 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Button" component={ButtonScreen} />
         <Stack.Screen name="RadioButton" component={RadioButtonScreen} />
+        <Stack.Screen name="CheckBox" component={ChexboxScreen} />
         <Stack.Screen name="NumPad" component={NumPadScreen} />
         <Stack.Screen name="Tables" component={TableScreen} />
         <Stack.Screen name="Popup" component={PopupScreen} />
