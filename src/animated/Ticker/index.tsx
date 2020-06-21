bb1190b33c9ca78e0fc962a34f6ad1550866e167
vinit_groww <vinitraj1012@gmail.com>
2020-06-21 14:57:44 +0530

    Ticker component init

---
+++ src/animated/Ticker/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Ticker extends React.PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>Number Ticker</Text>
+      </View>
+    );
+  }
+}
+
+export default Ticker;
--- src/animated/index.tsx
+++ src/animated/index.tsx
@@ -1,1 +1,2 @@
 export { default as Fade } from './Fade';
+export { default as Ticker } from './Ticker';
