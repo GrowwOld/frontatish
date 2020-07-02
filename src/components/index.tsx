6e09a3f8efe1dfe434ba11717bc4bf0e74779872
vinit_groww <vinitraj1012@gmail.com>
2020-07-02 13:32:38 +0530

    removed modal for alpha release

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,9 +2,8 @@ import Ripple from 'react-native-material-ripple';
 import Button from './Button';
 import Checkbox from './Checkbox';
 import NumPad from './NumPad';
-import Popup from './Popup';
 import RadioButton from './RadioButton';
 import Table from './Table';
 import Slider from './Slider';
 
-export { Button, Checkbox, NumPad, Popup, RadioButton, Table, Ripple, Slider };
+export { Button, Checkbox, NumPad, RadioButton, Table, Ripple, Slider };
