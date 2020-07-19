a24962faeef9c0b8173a5f5eccbf3c769fcee9c0
vinit_groww <vinitraj1012@gmail.com>
2020-07-19 18:27:15 +0530

    fixing calendar ui

--- example/src/examples/CalendarExample.tsx
+++ example/src/examples/CalendarExample.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line import/no-unresolved
 import { Calendar, useColors } from 'supergroww';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const CalendarExample = () => {
   const Colors = useColors();
+  const [date, setDate] = useState(new Date());
+  const setSelectedDate = (calendarDate: Date) => {
+    setDate(calendarDate);
+  };
   return (
     <SafeAreaView
       style={{
@@ -14,8 +18,9 @@ const CalendarExample = () => {
         backgroundColor: Colors.white,
       }}
     >
+      <Text>{date?.toString()}</Text>
       <View style={{ margin: 20 }}>
-        <Calendar />
+        <Calendar setDate={setSelectedDate} />
       </View>
     </SafeAreaView>
   );
