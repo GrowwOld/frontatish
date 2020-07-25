b6d23a51a8747f24a7b0cc5b0fa98138d09d2292
vinit_groww <vinitraj1012@gmail.com>
2020-07-25 20:56:03 +0530

    input component init

--- example/src/examples/InputExample.tsx
+++ example/src/examples/InputExample.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
-import { Input } from 'supergroww';
+import { Input, useColors } from 'supergroww';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const InputExample = () => {
+  const Colors = useColors();
   return (
-    <SafeAreaView>
-      <Input />
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.white }}>
+      <Input label="Name (As on PAN Card)" placeholder="CGBPR3376M" />
     </SafeAreaView>
   );
 };
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,97 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  TextInput,
+  TextInputProps,
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import { withColors } from '../../themes';
+import { StyleType, ColorType } from '../../common/types';
 
-class Input extends React.PureComponent {
+let styles: any;
+interface InputProps extends TextInputProps {
+  label?: string;
+  error?: string;
+  disabled?: string;
+  disabledStyle: StyleType;
+  containerStyle: StyleType;
+  labelStyle: StyleType;
+  bottomBorderStyle?: StyleType;
+  Colors?: any;
+}
+
+interface InputState {
+  active: boolean;
+}
+
+class Input extends React.PureComponent<InputProps, InputState> {
+  constructor(props: InputProps) {
+    super(props);
+    this.state = { active: false };
+  }
+
+  setFocus = (active: boolean) => {
+    this.setState({ active });
+  };
+
+  getBottomBorderStyles = () => {
+    const { bottomBorderStyle, Colors } = this.props;
+    const { active } = this.state;
+    let mergedStyle;
+    if (active) {
+      mergedStyle = [
+        bottomBorderStyle,
+        {
+          height: 2,
+          backgroundColor: Colors.primary,
+        },
+      ];
+    } else {
+      mergedStyle = [
+        bottomBorderStyle,
+        {
+          height: 1,
+          backgroundColor: Colors.font_3,
+        },
+      ];
+    }
+    return mergedStyle;
+  };
+
   render() {
+    const {
+      label,
+      containerStyle,
+      placeholder,
+      labelStyle,
+      Colors,
+    } = this.props;
+    styles = getStyles(Colors);
+    const bottomBorderStyles = this.getBottomBorderStyles();
     return (
-      <View>
-        <Text>Input Component</Text>
+      <View style={[styles.containerStyle, containerStyle]}>
+        <Text style={[styles.lableStyle, labelStyle]}>{label}</Text>
+        <TextInput
+          placeholder={placeholder}
+          onFocus={() => this.setFocus(true)}
+          onBlur={() => this.setFocus(false)}
+        />
+        <View style={bottomBorderStyles} />
       </View>
     );
   }
 }
 
-export default Input;
+const getStyles = (Colors: ColorType) => {
+  return StyleSheet.create({
+    containerStyle: { padding: 20 },
+    lableStyle: { color: Colors.font_2 },
+    bottomBorder: {
+      height: 2,
+      marginVertical: 4,
+    },
+  });
+};
+
+export default withColors(Input);
