abdf9ca9e4f520fce9e5aeac922050ad03830ad7
vinit_groww <vinitraj1012@gmail.com>
2020-08-01 10:34:33 +0530

    removed slider from alpha release

--- example/src/examples/navigation/index.tsx
+++ example/src/examples/navigation/index.tsx
@@ -3,10 +3,7 @@ import CheckboxScreen from '../CheckboxExample';
 import RadioButtonScreen from '../RadioButtonExample';
 import NumPadScreen from '../NumPadExample';
 import TableScreen from '../TableExample';
-// removing for now,we can have more customization of it later
-// import PopupScreen from '../PopupExample';
 import FadeScreen from '../FadeExample';
-import SliderScreen from '../SliderExample';
 import CalendarScreen from '../CalendarExample';
 
 export const COMPONENT_SCREENS = {
@@ -15,9 +12,6 @@ export const COMPONENT_SCREENS = {
   RADIOBUTTON: 'Radiobutton',
   NUMPAD: 'Numpad',
   TABLES: 'Tables',
-  // SWITCH: 'Switch',
-  // POPUP: 'Popup',
-  SLIDER: 'Slider',
   CALENDAR: 'Calendar',
 };
 
@@ -30,9 +24,6 @@ export const SCREEN_MAPPING = [
   { name: COMPONENT_SCREENS.RADIOBUTTON, screen: RadioButtonScreen },
   { name: COMPONENT_SCREENS.NUMPAD, screen: NumPadScreen },
   { name: COMPONENT_SCREENS.TABLES, screen: TableScreen },
-  // { name: COMPONENT_SCREENS.SWITCH, screen: SwitchScreen },
-  // { name: COMPONENT_SCREENS.POPUP, screen: PopupScreen },
-  { name: COMPONENT_SCREENS.SLIDER, screen: SliderScreen },
   { name: COMPONENT_SCREENS.CALENDAR, screen: CalendarScreen },
   // animated screen mapping
   { name: ANIMATED_SCREENS.FADE, screen: FadeScreen },
