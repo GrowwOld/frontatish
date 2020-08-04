580cebbfdccfc99ee3a2b8c5174cbe24989ee9b3
vinit_groww <vinitraj1012@gmail.com>
2020-08-04 11:09:21 +0530

    types fixes

--- src/themes/ThemeContext.tsx
+++ src/themes/ThemeContext.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { ColorType } from '../common/types';
 
-const ThemeContext = React.createContext('light');
+interface conttextObjType {
+  currentTheme: string;
+  toggleTheme: (theme: string) => void;
+  colors?: ColorType;
+}
+const ThemeContext = React.createContext<conttextObjType>({
+  currentTheme: 'light',
+  toggleTheme: () => {},
+});
 
 export default ThemeContext;
