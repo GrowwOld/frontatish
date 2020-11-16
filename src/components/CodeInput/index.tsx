dac2a67aa3bae6c9bec3940468e0f5e2685d5b83
vinit_groww <vinit.raj@groww.in>
2020-11-16 23:57:28 +0530

    animation finished

--- src/components/CodeInput/index.tsx
+++ src/components/CodeInput/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Animated } from 'react-native';
 // eslint-disable-next-line import/no-unresolved
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { withColors } from '../../themes';
@@ -22,7 +22,8 @@ const styles = StyleSheet.create({
     height: 50,
     borderWidth: 2,
     borderColor: 'black',
-    justifyContent: 'space-around',
+    marginRight: 30,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   inputUnderlined: {
@@ -30,19 +31,24 @@ const styles = StyleSheet.create({
     width: 50,
     borderBottomColor: 'black',
     borderBottomWidth: 2,
-    justifyContent: 'space-around',
+    marginRight: 30,
+    justifyContent: 'center',
     alignItems: 'center',
   },
 });
 
 class CodeInput extends React.PureComponent<CodeInputProps, CodeInputState> {
+  animatedValue: Animated.Value;
+
   constructor(props: CodeInputProps) {
     super(props);
+    this.animatedValue = new Animated.Value(0);
     this.state = {
       prevValue: props.value,
       codeInputValue: [],
       activeInput: 0,
     };
+    // this.startAnimation();
   }
 
   static getDerivedStateFromProps(
@@ -65,6 +71,23 @@ class CodeInput extends React.PureComponent<CodeInputProps, CodeInputState> {
     return null;
   }
 
+  componentDidUpdate(_prevProps: CodeInputProps, prevState: CodeInputState) {
+    const { activeInput } = this.state;
+    const { codeLength } = this.props;
+    if (activeInput !== prevState.activeInput && activeInput < codeLength) {
+      this.startAnimation(activeInput);
+    }
+  }
+
+  startAnimation = (toValue: number) => {
+    console.log('toValue->', toValue, this.animatedValue);
+    Animated.spring(this.animatedValue, {
+      toValue,
+      useNativeDriver: true,
+      // speed: 3,
+    }).start(() => this.animatedValue.setValue(toValue));
+  };
+
   renderInputUI = () => {
     const { codeLength, Colors, inputContainer } = this.props;
     const { codeInputValue, activeInput } = this.state;
@@ -74,7 +97,7 @@ class CodeInput extends React.PureComponent<CodeInputProps, CodeInputState> {
       const activeStyle =
         inputContainer === 'line'
           ? { borderBottomColor: Colors.primary }
-          : { borderColor: Colors.primary };
+          : { borderColor: Colors.font_1 };
       ui.push(
         <TouchableWithoutFeedback
           style={[
@@ -99,8 +122,41 @@ class CodeInput extends React.PureComponent<CodeInputProps, CodeInputState> {
 
   render() {
     // const { value, codeLength } = this.props;
+    const { Colors, inputContainer } = this.props;
+    const { activeInput } = this.state;
+    const prevActiveInput = activeInput - 1;
+    const prevXDistance = prevActiveInput * 80;
+    const currentXDistance = activeInput * 80;
+    console.log(
+      'Data after input',
+      activeInput,
+      prevXDistance,
+      currentXDistance,
+    );
     return (
-      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+      <View style={{ flexDirection: 'row', marginHorizontal: 20 }}>
+        <Animated.View
+          style={[
+            inputContainer === 'line'
+              ? styles.inputUnderlined
+              : styles.inputBox,
+            {
+              position: 'absolute',
+              left: 0,
+              zIndex: 1,
+              borderBottomColor: Colors.primary,
+              borderColor: Colors.primary,
+              transform: [
+                {
+                  translateX: this.animatedValue.interpolate({
+                    inputRange: [activeInput - 1, activeInput],
+                    outputRange: [prevXDistance, currentXDistance],
+                  }),
+                },
+              ],
+            },
+          ]}
+        />
         {this.renderInputUI()}
       </View>
     );
