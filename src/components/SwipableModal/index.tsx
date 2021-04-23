dcc356c20bdd493beaaccce3f41d6ec3ee7aaaf7
AkashKait55 <akash.kait@groww.in>
2021-04-24 04:39:20 +0530

    added full width

--- src/components/SwipableModal/index.tsx
+++ src/components/SwipableModal/index.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import { SwipableModalProps } from './types';
 import Modal from 'react-native-modal';
 import { Button } from 'frontatish';
 import { ScrollView } from 'react-native-gesture-handler';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import Ripple from 'react-native-material-ripple';
+import Modalbox from 'react-native-modalbox';
+import { useColors } from '../../themes';
 
 const SwipableModal = (props: SwipableModalProps) => {
+  const Colors = useColors();
   const { title } = props;
   const [isOpen, setIsOpen] = useState(false);
 
@@ -20,6 +23,7 @@ const SwipableModal = (props: SwipableModalProps) => {
         margin: 30,
         flex: 1,
         justifyContent: 'center',
+        backgroundColor: 'red',
       }}
     >
       <Text>{title}</Text>
@@ -28,20 +32,39 @@ const SwipableModal = (props: SwipableModalProps) => {
         backdropOpacity={0.3}
         swipeDirection="down"
         animationOutTiming={500}
-        swipeThreshold={150}
+        swipeThreshold={Dimensions.get('window').height / 8}
         onSwipeComplete={closeModal}
         onBackdropPress={closeModal}
         onBackButtonPress={closeModal}
         propagateSwipe={true}
         coverScreen={true}
+        style={{
+          margin: 0,
+        }}
       >
+        {/* <Modalbox
+        isOpen={isOpen}
+        backButtonClose
+        onClosed={closeModal}
+        coverScreen
+        swipeThreshold={150}
+        style={{
+          borderRadius: 4,
+          maxHeight: 600,
+          bottom: 0,
+          position: 'absolute',
+        }}
+      > */}
         <View
           style={{
             padding: 5,
-            backgroundColor: '#fff',
+            flex: 1,
+            backgroundColor: Colors.white,
             borderRadius: 4,
-            marginTop: 'auto',
-            minHeight: 400,
+            bottom: 0,
+            position: 'absolute',
+            width: '100%',
+            maxHeight: Dimensions.get('window').height / 2,
           }}
         >
           <View
@@ -49,6 +72,7 @@ const SwipableModal = (props: SwipableModalProps) => {
               flexDirection: 'row',
               justifyContent: 'space-between',
               alignItems: 'center',
+              backgroundColor: Colors.font_6,
             }}
           >
             <View style={{ width: 32 }}></View>
@@ -56,7 +80,6 @@ const SwipableModal = (props: SwipableModalProps) => {
               style={{
                 borderWidth: 1,
                 width: 50,
-                marginBottom: 10,
               }}
             ></View>
             <Ripple
@@ -72,12 +95,21 @@ const SwipableModal = (props: SwipableModalProps) => {
             >
               <IonIcon
                 name="ios-close"
-                size={20}
+                size={24}
                 // style={{ flex: 1, textAlign: 'center' }}
               />
             </Ripple>
           </View>
+          {/* <ScrollView>
+            <Text style={{ flex: 1, fontSize: 100 }}>Hello</Text>
+            <Text style={{ flex: 1, fontSize: 100 }}>Hello</Text>
+            <Text style={{ flex: 1, fontSize: 100 }}>Hello</Text>
+            <Text style={{ flex: 1, fontSize: 100 }}>Hello</Text>
+            <Text style={{ flex: 1, fontSize: 100 }}>Hello</Text>
+            <Text style={{ flex: 1, fontSize: 100 }}>Hello</Text>
+          </ScrollView> */}
         </View>
+        {/* </Modalbox> */}
       </Modal>
       <Button label="Open Modal" onPress={() => setIsOpen(true)} />
     </View>
