195a17302137f5da852a6a2d6976e436a61e1b50
growwuser <growwuser@Shivangshus-MacBook-Air.local>
2021-06-05 14:42:54 +0530

    fix typo

--- src/themes/ThemeContext.tsx
+++ src/themes/ThemeContext.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ColorType } from '../common/types';
 
-interface conttextObjType {
+interface contextObjType {
   currentTheme: string;
   toggleTheme: (theme: string) => void;
   colors?: ColorType;
 }
-const ThemeContext = React.createContext<conttextObjType>({
+const ThemeContext = React.createContext<contextObjType>({
   currentTheme: 'light',
   toggleTheme: () => {},
 });
